35b051d035e95b95b84c27e2bccc6f6721fb4d2b
Rob Kaufman <rob.m.kaufman@gmail.com>
2010-03-25 07:29:56 -0400

    changed global key settings to a minor mode

--- global-keys.el
+++ global-keys.el
@@ -1,53 +1,58 @@
+(defvar kirkland-minor-mode-map (make-keymap) "kirkland-minor-mode keymap.")
+
 ;; basic movement and editing 
-(global-set-key (kbd "M-h") 'backward-char)
-(global-set-key (kbd "M-l") 'forward-char)
-(global-set-key (kbd "M-j") 'next-line)
-(global-set-key (kbd "M-k") 'previous-line)
+(define-key kirkland-minor-mode-map (kbd "M-h") 'backward-char)
+(define-key kirkland-minor-mode-map (kbd "M-l") 'forward-char)
+(define-key kirkland-minor-mode-map (kbd "M-j") 'next-line)
+(define-key kirkland-minor-mode-map (kbd "M-k") 'previous-line)
+(define-key kirkland-minor-mode-map (kbd "M-d") 'delete-backward-char)
+(define-key kirkland-minor-mode-map (kbd "M-f") 'delete-char)
 
-(global-set-key (kbd "M-d") 'delete-backward-char)
-(global-set-key (kbd "M-f") 'delete-char)
+(define-key kirkland-minor-mode-map (kbd "M-y") 'backward-word)
+(define-key kirkland-minor-mode-map (kbd "M-o") 'forward-word)
+(define-key kirkland-minor-mode-map (kbd "M-u") 'backward-kill-word)
+(define-key kirkland-minor-mode-map (kbd "M-i") 'kill-word)
 
-(global-set-key (kbd "M-y") 'backward-word)
-(global-set-key (kbd "M-o") 'forward-word)
-(global-set-key (kbd "M-u") 'backward-kill-word)
-(global-set-key (kbd "M-i") 'kill-word)
+(define-key kirkland-minor-mode-map (kbd "M-w") 'move-beginning-of-line)
+(define-key kirkland-minor-mode-map (kbd "M-e") 'move-end-of-line)
+(define-key kirkland-minor-mode-map (kbd "M-r") 'kill-line)
 
-(global-set-key (kbd "M-w") 'move-beginning-of-line)
-(global-set-key (kbd "M-e") 'move-end-of-line)
-(global-set-key (kbd "M-r") 'kill-line)
-
-(global-set-key (kbd "M-p") 'scroll-down)
-(global-set-key (kbd "M-n") 'scroll-up)
+(define-key kirkland-minor-mode-map (kbd "M-p") 'scroll-down)
+(define-key kirkland-minor-mode-map (kbd "M-n") 'scroll-up)
 
 ;; killing, yanking
-(global-set-key (kbd "M-SPC") 'set-mark-command)
-(global-set-key (kbd "M-s") (lambda () (interactive) (other-window 1)))
-(global-set-key (kbd "M-S") (lambda () (interactive) (other-window -1)))
-(global-set-key (kbd "M-x") 'kill-region)
-(global-set-key (kbd "M-c") 'kill-ring-save)
-(global-set-key (kbd "M-v") 'yank)
-(global-set-key (kbd "M-b") 'yank-pop)
+(define-key kirkland-minor-mode-map (kbd "M-SPC") 'set-mark-command)
+(define-key kirkland-minor-mode-map (kbd "M-s") (lambda () (interactive) (other-window 1)))
+(define-key kirkland-minor-mode-map (kbd "M-S") (lambda () (interactive) (other-window -1)))
+(define-key kirkland-minor-mode-map (kbd "M-x") 'kill-region)
+(define-key kirkland-minor-mode-map (kbd "M-c") 'kill-ring-save)
+(define-key kirkland-minor-mode-map (kbd "M-v") 'yank)
+(define-key kirkland-minor-mode-map (kbd "M-b") 'yank-pop)
 
 ;; files / buffers
-(global-set-key (kbd "C-o") 'find-file)
-(global-set-key (kbd "C-s") 'save-buffer)
-(global-set-key (kbd "C-b") 'bs-show)
-(global-set-key (kbd "C-w") 'close-current-buffer)
+(define-key kirkland-minor-mode-map (kbd "C-o") 'find-file)
+(define-key kirkland-minor-mode-map (kbd "C-s") 'save-buffer)
+(define-key kirkland-minor-mode-map (kbd "C-b") 'bs-show)
+(define-key kirkland-minor-mode-map (kbd "C-w") 'close-current-buffer)
 
 ;; windows
-(global-set-key (kbd "M-0") 'delete-window)
-(global-set-key (kbd "M-1") 'delete-other-windows)
-(global-set-key (kbd "M-2") 'split-window-vertically)
-(global-set-key (kbd "M-3") 'split-window-horizontally)
+(define-key kirkland-minor-mode-map (kbd "M-0") 'delete-window)
+(define-key kirkland-minor-mode-map (kbd "M-1") 'delete-other-windows)
+(define-key kirkland-minor-mode-map (kbd "M-2") 'split-window-vertically)
+(define-key kirkland-minor-mode-map (kbd "M-3") 'split-window-horizontally)
 
 ;; misc
-(global-set-key (kbd "M-/") 'undo)
-(global-set-key (kbd "M-;") 'isearch-forward)
-(global-set-key (kbd "M-:") 'isearch-backward)
-(global-set-key (kbd "M-t") 'dabbrev-expand)
-(global-set-key (kbd "M-a") 'execute-extended-command)
-(global-set-key (kbd "<f5>") (lambda () (interactive) (load-file "~/.emacs.d/init.el")))
+(define-key kirkland-minor-mode-map (kbd "M-/") 'undo)
+(define-key kirkland-minor-mode-map (kbd "M-;") 'isearch-forward)
+(define-key kirkland-minor-mode-map (kbd "M-:") 'isearch-backward)
+(define-key kirkland-minor-mode-map (kbd "M-t") 'dabbrev-expand)
+(define-key kirkland-minor-mode-map (kbd "M-a") 'execute-extended-command)
+(define-key kirkland-minor-mode-map (kbd "<f5>") (lambda () (interactive) (load-file "~/.emacs.d/init.el")))
 
-(global-set-key (kbd "C-x C-b") 'bs-show) ; use buffer-selection-menu mode
-(global-set-key (kbd "C-z") 'shell)
-(global-set-key (kbd "C-c y") 'clipboard-yank)
+(define-key kirkland-minor-mode-map (kbd "C-x C-b") 'bs-show) ; use buffer-selection-menu mode
+(define-key kirkland-minor-mode-map (kbd "C-z") 'shell)
+(define-key kirkland-minor-mode-map (kbd "C-c y") 'clipboard-yank)
+
+(define-minor-mode kirkland-minor-mode
+  "A minor mode so that my key settings aren't shadowed by other major/minor modes"
+  t " kirkland" 'kirkland-minor-mode-map)
