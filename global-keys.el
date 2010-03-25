74449885f2438d7f585957b73385de39497631d9
Rob Kaufman <rob.m.kaufman@gmail.com>
2010-03-25 07:22:06 -0400

    M-0 like C-x 0

--- global-keys.el
+++ global-keys.el
@@ -35,6 +35,7 @@
 (global-set-key (kbd "C-w") 'close-current-buffer)
 
 ;; windows
+(global-set-key (kbd "M-0") 'delete-window)
 (global-set-key (kbd "M-1") 'delete-other-windows)
 (global-set-key (kbd "M-2") 'split-window-vertically)
 (global-set-key (kbd "M-3") 'split-window-horizontally)
