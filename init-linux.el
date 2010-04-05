e835c2ba2130c37f0235cdb0dba41e9620490c5f
rkaufman <rkaufman@interreport.com>
2010-04-05 10:39:10 -0400

    fixed color theme for version 22

--- init-linux.el
+++ init-linux.el
@@ -12,7 +12,8 @@
 (require 'color-theme)
 (color-theme-initialize)
 (if (or (eql nil (getenv "SSH_CLIENT"))
-	(eql emacs-major-version 21))   ;; hack for deutsch...
+	(eql emacs-major-version 21)
+	(eql emacs-major-version 22))
     (color-theme-arjen)
   (if (eq system-type 'gnu/linux)
       (color-theme-blue-mood)))
