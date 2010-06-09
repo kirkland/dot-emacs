b9252779dd6c8da4ddb8bcf2d60273b3bfb00b75
Robert Kaufman <rkaufman@digitaladvisor.com>
2010-06-09 17:33:58 -0400

    added package.el

--- init.el
+++ init.el
@@ -205,3 +205,15 @@
            (gnuserv-start))
   (when (not (eq 'windows-nt system-type)) (server-start)))
 
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+;;; run package-list-packages
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
