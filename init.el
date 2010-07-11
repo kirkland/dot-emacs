4f297868cf87c6e44aac5548fd3a931b8266ea76
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-07-11 18:08:51 -0400

    remove many bindings not necessary w/ viper mode

--- init.el
+++ init.el
@@ -17,23 +17,6 @@
 
 (if (eql system-type 'windows-nt) (load-file (concat emacs-root "windows-init.el")))
 
-;; i think this only works because it appears before loading of custom file
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
- '(font-lock-builtin-face ((((type tty) (class color)) (:foreground "yellow" :weight light))))
- '(link ((((class color) (background light)) (:foreground "cyan" :underline t))))
- '(minibuffer-prompt ((t (:foreground "cyan" :family "default")))))
-;; colors
-(require 'color-theme)
-(color-theme-initialize)
-
-(global-set-key (kbd "C-c t 1") (lambda () (interactive) (color-theme-arjen)))
-(global-set-key (kbd "C-c t 2") (lambda () (interactive) (color-theme-blue-mood)))
-
 (defconst emacs-scratch (concat (getenv "HOME") "/.scratch-emacs/"))
 (make-directory emacs-scratch t)
 
