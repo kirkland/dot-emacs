2b04707a807d35bfb8e5f7ea2ea4abd76f7efe9f
Robert Kaufman <rkaufman@digitaladvisor.com>
2010-07-12 15:39:42 +0000

    add colors back in

--- viper.el
+++ viper.el
@@ -1,2 +1,5 @@
 (setq viper-inhibit-startup-message 't)
 (setq viper-expert-level '5)
+
+(define-key viper-vi-global-user-map (kbd "C-b") 'ido-switch-buffer)
+(define-key viper-vi-global-user-map (kbd "C-o") 'find-file)
