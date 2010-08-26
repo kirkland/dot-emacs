03017efde969bf1b9c4bf4345304d1fb29fed322
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-08-25 22:01:29 -0400

    bind M-q to close other windows

--- my_bindings.el
+++ my_bindings.el
@@ -49,17 +49,17 @@
 (global-set-key (kbd "M-m") 'end-of-buffer)
 
 ;; killing, yanking
-(global-set-key (kbd "M-v") 'yank)
-(global-set-key (kbd "M-b") 'yank-pop)
-(global-set-key (kbd "M-c") 'kill-ring-save)
+;(global-set-key (kbd "M-v") 'yank)
+;(global-set-key (kbd "M-b") 'yank-pop)
+;(global-set-key (kbd "M-c") 'kill-ring-save)
 (global-set-key (kbd "M-SPC") 'set-mark-command)
 (global-set-key (kbd "M-RET") 'newline)
-(global-set-key (kbd "M-x") 'kill-region)
+;(global-set-key (kbd "M-x") 'kill-region)
 (global-set-key (kbd "M-4") 'expand-region-linewise)
 
 ;; windows and buffers
 (global-set-key (kbd "M-0") 'delete-window)
-(global-set-key (kbd "M-1") 'delete-other-windows)
+(global-set-key (kbd "M-w") 'delete-other-windows)
 (global-set-key (kbd "C-c 1") 'delete-other-windows)
 (global-set-key (kbd "M-2") 'split-window-vertically)
 (global-set-key (kbd "M-3") 'split-window-horizontally)
