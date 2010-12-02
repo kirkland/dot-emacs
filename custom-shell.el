1b141c2663ba205e2e3ec7ebcb09626598023436
Robert Kaufman <rkaufman@digitaladvisor.com>
2010-12-02 21:43:17 +0000

    i told you i was hardcore

--- custom-shell.el
+++
@@ -1,4 +0,0 @@
-; (add-hook 'shell-mode-hook
-; 	  '(lambda () (local-set-key "\M-." 'comint-insert-previous-argument)))
-
-(define-key shell-mode-map "\M-." 'comint-insert-previous-argument)
--- integ-mode.el
+++
@@ -1,8 +0,0 @@
-;;;; integ mode
-
-(define-generic-mode integ-mode
-  '("//")
-  '("object")
-  '(("TASK\\|TSKL\\|PARM\\|INPT\\|PROC\\|OUTP" . 'font-lock-variable-name-face))
-  '(".int\\'")
-  nil)
